06d8125c1e7843d6d5704017d13ee3898b25dc53
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-09 19:11:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,29 +1,17 @@
 def third_challenge
+
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :patriarch => {name: "Lord Montague", age: "53"},
-        },
-      :matriarch => {
-        :matriarch => {name: "Lady Montague", age: "54"},
-        },
-      :hero => {
-        :hero => {name: "Romeo", age: "15", status: "alive"},
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        :patriarch => {name: "Lord Capulet", age: "50"},
-        },
-      :matriarch => {
-        :matriarch => {name: "Lady Capulet", age: "51"},
-        },
-      :heroine => {
-        :heroine => {name: "Juliette", age: "15", status: "alive"},
-        },
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliette", age: "15", status: "alive"},
       :heroine_friends => []
    }
   }
-
 end
