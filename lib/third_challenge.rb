25789d0db7e6f7b2dd7efc320de04107617f9b9a
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-09 19:09:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :patriarch => {name: "Lord Montague", age: "53"},
         },
       :matriarch => {
-        #your key/value pairs here
+        :matriarch => {name: "Lady Montague", age: "54"},
         },
       :hero => {
         #your key/value pairs here
